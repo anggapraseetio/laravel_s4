17f333710d96f4afa87627dfefd885be2bd85f42
anggapraseetio <e41231567@student.polije.ac.id>
2025-03-10 22:50:42 +0700

    acara 20

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -72,5 +72,22 @@ class UploadController extends Controller
 
         return redirect(route('upload.resize'))->with('success', 'Data berhasil ditambahkan!');
     }
-    
+    public function dropzone(){
+        return view('dropzone');
+    }
+    public function dropzone_store(Request $request){
+        $image = $request->file('file');
+        $imageName = time().'.'.$image->extension();
+        $image->move(public_path('img/dropzone'), $imageName);
+        return response()->json(['success'=> $imageName]);
+    }
+    public function pdf_upload(){
+        return view('pdf_upload');
+    }
+    public function pdf_store(Request $request){
+        $pdf =$request->file('file');
+        $pdfName = 'pdf_'.time().'.'.$pdf->extension();
+        $pdf->move(public_path('pdf/dropzone'), $pdfName);
+        return response()->json(['succes' => $pdfName]);
+    }
 }
